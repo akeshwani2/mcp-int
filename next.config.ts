7fd86eef20653efddbc194f5229baa33742964d3
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2025-03-31 22:15:14 -0400

    deploy test #7

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Fix for @prisma/client module resolution
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@prisma/client': require.resolve('@prisma/client')
+      };
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
